bad7d8773881ff68a0fe644f4a8c5fc78a48d81c
kyu3638 <kyu3638@gmail.com>
2025-08-21 15:14:22 +0900

    chore. eslint, prettier 설정

---
+++ eslint.config.js
@@ -0,0 +1,32 @@
+import eslint from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import prettierConfig from 'eslint-config-prettier';
+
+export default [
+  eslint.configs.recommended,
+  {
+    files: ['**/*.{ts,tsx}'],
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    },
+  },
+  prettierConfig,
+  {
+    ignores: ['dist/', 'node_modules/', '*.config.js'],
+  },
+];
