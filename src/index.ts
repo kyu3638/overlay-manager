81ec04d8d9ce8c22ff281b31cab2faaf0ff24cde
kyu3638 <kyu3638@gmail.com>
2025-08-21 20:31:11 +0900

    fix. export 경로 수정

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { default as OverlayProvider } from './OverlayProvider';
 export { default as OverlayContext } from './OverlayContext';
-export { default as useOverlayContext } from './OverlayProvider';
+export { default as useOverlayContext } from './useOverlayContext';
 export type { OverlayItem, OverlayContextValue } from './types';
